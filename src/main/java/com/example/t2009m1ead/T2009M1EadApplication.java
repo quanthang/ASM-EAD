077c84e379ea42958c8f33708e7e8262172da051
unknown <quan@gmail.com>
2022-07-30 16:01:46 +0800

    update

---
+++ src/main/java/com/example/t2009m1ead/T2009M1EadApplication.java
@@ -0,0 +1,13 @@
+package com.example.t2009m1ead;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2009M1EadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(T2009M1EadApplication.class, args);
+    }
+
+}
